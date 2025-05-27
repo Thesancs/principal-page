f5abc97d0801eb05ae6c1084ac307c6832ac54ff
Thesancs <santos.glauber2002@gmail.com>
2025-05-27 12:16:27 -0300

    Atualização o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+// tailwind.config.js (CommonJS)
+module.exports = {
+  darkMode: 'class',
+
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#FF0033',
+          dark:    '#E6002E',
+        },
+        'dark-bg': '#000000',
+      },
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
       },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
     },
   },
-plugins: [
-  require('@tailwindcss/aspect-ratio'),
-],
 
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
